18fb152c3708df4f7a41862111fc8f5294ec5dc6
maheshkarbhardev <maheshkarbhar.dev@gmail.com>
2024-06-17 20:58:23 +0530

    Initial commit for frontend

---
+++ src/pages/AddEdit.js
@@ -0,0 +1,136 @@
+import React, { useState, useEffect } from "react";
+import { useNavigate, useParams, Link } from "react-router-dom";
+import "./AddEdit.css";
+import axios from "axios";
+import { toast } from "react-toastify";
+
+const initialState = {
+  name: "",
+  email: "",
+  contact: "",
+};
+
+const AddEdit = () => {
+  const [state, setState] = useState(initialState);
+  const { name, email, contact } = state;
+
+  const navigate = useNavigate();
+
+  const { id } = useParams();
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:5000/api/get/${id}`)
+      .then((resp) => setState({ ...resp.data[0] }));
+  }, [id]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!name || !email || !contact) {
+      toast.error("Please Fill All Data");
+    } else {
+      if (!id) {
+        axios
+          .post("http://localhost:5000/api/post", {
+            name,
+            email,
+            contact,
+          })
+          .then(() => {
+            setState({
+              name: "",
+              email: "",
+              contact: "",
+            });
+          })
+          .catch((err) => {
+            toast.error(err.response.data);
+          });
+        toast.success("Contact Added Successfully.");
+      } 
+      else {
+        axios
+          .put(`http://localhost:5000/api/update/${id}`, {
+            name,
+            email,
+            contact,
+          })
+          .then(() => {
+            setState({
+              name: "",
+              email: "",
+              contact: "",
+            });
+          })
+          .catch((err) => {
+            toast.error(err.response.data);
+          });
+        toast.success("Contact Updated Successfully.");
+      }
+
+      setTimeout(() => {
+        navigate("/");
+      }, 500);
+    }
+  };
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setState({ ...state, [name]: value });
+  };
+
+  return (
+    <div style={{ marginTop: "100px" }}>
+      <form
+        action=""
+        style={{
+          margin: "auto",
+          padding: "15px",
+          maxWidth: "400px",
+          alignContent: "center",
+        }}
+        onSubmit={handleSubmit}
+      >
+        <label htmlFor="name">Name</label>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          placeholder="Enter Name"
+          value={name || ""}
+          onChange={handleInputChange}
+        />
+
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          placeholder="Enter Email"
+          value={email || ""}
+          onChange={handleInputChange}
+        />
+
+        <label htmlFor="contact">Contact</label>
+        <input
+          type="number"
+          id="contact"
+          name="contact"
+          placeholder="Enter Contact Number"
+          value={contact || ""}
+          onChange={handleInputChange}
+        />
+
+        <button className="btn-submit" value={id ? "Update" : "Save"}>
+          Save
+        </button>
+
+        <Link to="/">
+          <button className="btn-goback">Go Back</button>
+        </Link>
+      </form>
+    </div>
+  );
+};
+
+export default AddEdit;
